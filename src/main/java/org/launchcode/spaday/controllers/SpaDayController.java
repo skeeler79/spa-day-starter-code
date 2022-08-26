bec2c06a26c6807110da50569008461f4caaed07
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-25 21:03:32 -0400

    finished studio and started bonus

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -63,6 +63,12 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", appropriateFacials);
+
+
         return "menu";
     }
 }
